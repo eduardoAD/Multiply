f98060e61debbd9325b7ee8e4a872394ba606835
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-07 13:10:16 -0500

    Initial Commit

---
+++ Multiply/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Multiply
+//
+//  Created by Eduardo Alvarado Díaz on 10/7/14.
+//  Copyright (c) 2014 Globant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
