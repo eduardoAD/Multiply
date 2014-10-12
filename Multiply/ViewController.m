2c0a9cd04981c4ea4ffc3d69e636f08e5b06315b
Eduardo Alvarado Diaz <e.alvaradod@gmail.com>
2014-10-11 20:06:23 -0500

    task #9 complete, added switch the operator

--- Multiply/ViewController.m
+++ Multiply/ViewController.m
@@ -13,6 +13,7 @@
 @property (strong, nonatomic) IBOutlet UILabel *multiplierLabel;
 @property (strong, nonatomic) IBOutlet UILabel *answerLabel;
 @property (strong, nonatomic) IBOutlet UISlider *sliderField;
+@property (strong, nonatomic) IBOutlet UISegmentedControl *operatorSegmentControl;
 
 @end
 
@@ -30,34 +31,43 @@
 - (IBAction)onCalculateButtonPressed:(id)sender {
     int number = [self.numberTextField.text intValue];
     int multiplier = [self.multiplierLabel.text intValue];
+    int result = 0;
 
-    int result = number * multiplier;
+    if (self.operatorSegmentControl.selectedSegmentIndex == 0) {
+        result = multiplier * number;
+    }else{
+        if (number > 0) {
+            result = multiplier / number;
+        }
+    }
 
     self.answerLabel.text = @"";
+    NSMutableString *fizzBuzzText = [[NSMutableString alloc] initWithString:@(result).description];
+
     if (result >= 20) {
         self.view.backgroundColor = [UIColor colorWithRed:0.361 green:0.722 blue:0.361 alpha:1];
     }else{
         self.view.backgroundColor = [UIColor whiteColor];
     }
 
-    NSMutableString *fizzBuzzText = [[NSMutableString alloc] initWithString:@""];
     if (result > 0) {
         if ((result % 3) == 0) {
-            [fizzBuzzText appendString:@"fizz"];
+            [fizzBuzzText appendString:@" fizz"];
         }
         if ((result % 5) == 0) {
-            [fizzBuzzText appendString:@"buzz"];
-        }
-        if ([fizzBuzzText isEqualToString:@""]) {
-            [fizzBuzzText appendString:@(result).description];
+            [fizzBuzzText appendString:@" buzz"];
         }
         self.answerLabel.text = fizzBuzzText;
     }
 }
 
-
 - (IBAction)sliderValueChanged:(id)sender {
     self.multiplierLabel.text = [NSString stringWithFormat:@"%.0f", self.sliderField.value];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [self onCalculateButtonPressed:textField];
+    return NO;
 }
 
 @end
